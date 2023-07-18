698ab1c59963aac04318e9b28f07db58d174da10
mobius <mobius@mobius.mobius>
2023-07-18 13:00:40 -0700

    significant refactor

--- sandbox/t11_document_chat.py
+++ sandbox/t11_document_chat.py
@@ -19,36 +19,21 @@ import numpy as np
 from tqdm import tqdm
 from scipy.signal import savgol_filter
 import pickle
+from dataclasses import dataclass
 
-WINDOW_SIZE = 300
-STRIDE = 100
-PERCENTILE = 80
-
-pdf_paths = {
-    'bitcoin': '~/Documents/misc/bitcoin.pdf',
-    'idaho': '~/Documents/misc/land use and development code.pdf'
-}
-
-
-# cache = [
-#     {
-#         'bitcoin': '~/Documents/misc/bitcoin.pdf',
-#         'window_size': 300,
-#         'stride': 100,
-#         'percentile': 80,
-#         'pdf_text': None,
-#         'embedding': None,
-#     },
-#     {
-#         'idaho': '~/Documents/misc/land use and development code.pdf',
-#         'window_size': 300,
-#         'stride': 100,
-#         'percentile': 80,
-#         'pdf_text': None,
-#         'embedding': None,
-#     }
-# ]
-
+@dataclass
+class Meta:
+    name: str
+    path: str
+    window_size: int
+    stride: int
+    percentile: int
+    pdf_text: str
+    embeddings: List[np.ndarray]
+    query_instruction: str
+    embed_instruction: str
+    denoise_window_size: int
+    denoise_poly_order: int
 
 ##################################################
 # Load Model
@@ -59,8 +44,6 @@ except:
     model = INSTRUCTOR('hkunlp/instructor-base')
     already_loaded = True
 
-query_instruction = 'Represent the Science question for retrieving supporting documents: '
-embed_instruction = 'Represent the Science document for retrieval: '
 
 
 def embed(xs: List[str]):
@@ -71,13 +54,16 @@ def embed(xs: List[str]):
 ##################################################
 # PDF
 
-def read_pdf(path):
-    path = os.path.expanduser(path)
-    reader = PdfReader(path)
-    text = ''
-    for page in reader.pages:
-        text += '\n' + page.extract_text()
-    return text
+def load_pdf(meta: Meta):
+    ''' Mutate `meta` to include `pdf_text` '''
+    if meta.pdf_text is None:
+        path = meta.path
+        path = os.path.expanduser(path)
+        reader = PdfReader(path)
+        text = ''
+        for page in reader.pages:
+            text += '\n' + page.extract_text()
+        meta.pdf_text = text
 
 
 def get_file_name(path):
@@ -102,48 +88,58 @@ def save_pkl(pdf_key, xs):
     print(f'Saved: {path}')
 
 
-def embed_documents(pdf_cache):
-    print('Preparing Embeddings')
-    embedding_cache = {}
-    for pdf_key, document in pdf_cache.items():
-        # Try to load embeddings from disk
-        embeddings = load_pkl(pdf_key)
-        if embeddings is not None:
-            print(f'Loaded {pdf_key} embeddings')
-            embedding_cache[pdf_key] = embeddings
-        else:  # If not found, then calculate
-            print(f'Preparing embeddings for {pdf_key}')
-            embeddings = []
-            # Loop through the document with given stride
-            offsets = list(enumerate(range(0, len(document) - WINDOW_SIZE + 1, STRIDE)))
-            for emb_i, doc_i in tqdm(offsets):
-                # Extract the chunk from document
-                chunk = document[doc_i:doc_i+WINDOW_SIZE]
-                # Embed the chunk
-                chunk_e = embed([[embed_instruction, chunk]])
-                embeddings.append(chunk_e)
+def load_embeddings(meta):
+    ''' Mutate `meta` to include embeddings. '''
+    pdf_key = meta.name
+    document = meta.pdf_text
+    window_size = meta.window_size
+    stride = meta.stride
+    embed_instruction = meta.embed_instruction
 
-            embedding_cache[pdf_key] = embeddings
-            save_pkl(pdf_key, embeddings)
-    return embedding_cache
+    # Try to load embeddings from disk
+    embeddings = load_pkl(pdf_key)
+    if embeddings is not None:
+        print(f'Loaded {pdf_key} embeddings')
+        meta.embeddings = embeddings
+    else:  # If not found, then calculate
+        print(f'Preparing embeddings for {pdf_key}')
+        embeddings = []
+        # Loop through the document with given stride
+        offsets = list(
+            enumerate(range(0, len(document) - window_size + 1, stride)))
+        for emb_i, doc_i in tqdm(offsets):
+            # Extract the chunk from document
+            chunk = document[doc_i:doc_i+window_size]
+            # Embed the chunk
+            chunk_e = embed([[embed_instruction, chunk]])
+            embeddings.append(chunk_e)
+
+        meta.embedding = embeddings
+        save_pkl(pdf_key, embeddings)
 
 
-def similar_tokens(query: str, embeddings: List[np.ndarray]) -> List[float]:
+def similar_tokens(query: str, meta: Meta) -> List[float]:
     '''Compare a `query` to a strided window over a `document`.'''
+    embeddings = meta.embeddings
+    pdf_key = meta.name
+    document = meta.pdf_text
+    window_size = meta.window_size
+    stride = meta.stride
+    query_instruction = meta.query_instruction
     # Initialize a numpy array for storing similarities and overlaps
-    document_length = len(embeddings) * STRIDE + WINDOW_SIZE - 1  # Derive the document length from embeddings
+    document_length = len(embeddings) * stride + window_size - 1  # Derive the document length from embeddings
     similarities = np.zeros(document_length, dtype=float)
     overlaps = np.zeros(document_length, dtype=float)
 
     query_e = embed([[query_instruction, query]])
 
     # Loop through the document with given stride
-    offsets = list(range(0, document_length - WINDOW_SIZE + 1, STRIDE))
+    offsets = list(range(0, document_length - window_size + 1, stride))
     for chunk_e, doc_i in tqdm(zip(embeddings, offsets)):
         sim = cosine_similarity(query_e, chunk_e)[0][0]
 
         # Update the similarities and overlaps array
-        for j in range(doc_i, doc_i + WINDOW_SIZE):
+        for j in range(doc_i, doc_i + window_size):
             similarities[j] += sim
             overlaps[j] += 1
 
@@ -151,19 +147,6 @@ def similar_tokens(query: str, embeddings: List[np.ndarray]) -> List[float]:
     similarities /= np.where(overlaps != 0, overlaps, 1)
     return similarities
 
-try:
-    pdf_cache
-except:
-    pdf_cache = {}
-
-    print('Loading PDF Text')
-    for k, path in pdf_paths.items():
-        pdf_cache[k] = read_pdf(path)
-
-try:
-    embedding_cache
-except:
-    embedding_cache = embed_documents(pdf_cache)
 
 def find_spans(arr, threshold=0.5):
     ''' '''
@@ -184,15 +167,6 @@ def tune_percentile(xs, percentile):
     p = np.percentile(xs, percentile)
     xs[xs < p] *= 0
     return xs
-
-
-def find_similar(query, pdf_key):
-    '''
-    query: a query that you want to find similar passages to
-    pdf_key: {bitcoin, idaho}
-    '''
-    embeddings = embedding_cache[pdf_key]
-    return similar_tokens(query, embeddings)
 
 
 def segments(similarities, document, threshold=0.0):
@@ -227,7 +201,7 @@ def rank(segments, rank_fn):
     for text, sims in segments:
         scores.append(rank_fn(sims))
     out = []
-    for score, (text, sims) in sorted(zip(scores, segments)):
+    for score, (text, sims) in reversed(sorted(zip(scores, segments))):
         out.append(text)
     return out
 
@@ -237,34 +211,89 @@ def denoise_similarities(similarities, window_size=2000, poly_order=2):
     return savgol_filter(similarities, window_size, poly_order)
 
 
+def top_segments(query, pdf_name, top_n):
+    meta = cache[pdf_name]
+    document = meta.pdf_text
+    denoise_window_size = meta.denoise_window_size
+    denoise_poly_order = meta.denoise_poly_order
+    percentile = meta.percentile
+    similarities = similar_tokens(query, meta)
+
+    # remove outlier at end
+    last_edge = int(len(similarities) * 0.01)
+    similarities[-last_edge:] = similarities[-last_edge]
+
+    # Denoise salience scores
+    similarities = tune_percentile(similarities, percentile)
+    d_similarities = denoise_similarities(similarities, denoise_window_size, denoise_poly_order)
+    d_similarities -= d_similarities.min()  # normalize
+    d_similarities /= d_similarities.max()
+    d_similarities = tune_percentile(d_similarities, percentile)
+
+    segs = segments(d_similarities, document)
+    ranked_segments = rank(segs, np.mean)[:top_n]
+
+    import matplotlib.pyplot as plt
+    plt.plot(similarities)
+    plt.plot(d_similarities)
+    plt.show()
+
+    return ranked_segments
+
+
+
+try:
+    cache_is_loaded
+except:
+    print('Populating cache')
+    cache = {
+        'bitcoin': Meta(
+            name='bitcoin',
+            path='~/Documents/misc/bitcoin.pdf',
+            window_size=300,
+            stride=100,
+            pdf_text=None,
+            embeddings=None,
+            query_instruction='Represent the Science question for retrieving supporting documents: ',
+            embed_instruction='Represent the Science document for retrieval: ',
+            denoise_window_size=2000,
+            denoise_poly_order=2,
+            percentile=80,
+        ),
+        'idaho': Meta(
+            name='idaho',
+            path='~/Documents/misc/land use and development code.pdf',
+            window_size=300,
+            stride=100,
+            pdf_text=None,
+            embeddings=None,
+            query_instruction='Represent the wikipedia question for retrieving supporting documents: ',
+            embed_instruction='Represent the wikipedia document for retrieval: ',
+            denoise_window_size=5000,
+            denoise_poly_order=2,
+            percentile=80,
+        )
+    }
+    for k, m in cache.items():
+        load_pdf(m)
+        load_embeddings(m)
+    cache_is_loaded = True
+
+# pdf_name = 'bitcoin'
 # query = 'whats in it for participants to the blockchain?'
 # query = 'how does this protect my anonymity?'
 # query = 'im concerned my hdd isnt big enough'
 # query = 'who contributed to this paper?'
 
-query = 'how close can my silver mine be to a farm?'
-pdf_key = 'idaho'
-document = pdf_cache[pdf_key]
-similarities = find_similar(query, pdf_key)
+pdf_name = 'idaho'
+# query = 'how close can my silver mine be to a farm?'
+# query = 'how do houses on the lake need to be addressed? marine addressing.'
+# query = 'How can I rezone my property? Rezoning.'
+# query = 'What signs can I put on my property?'
+query = 'How does this document define "sign"?'
 
-# remove outlier at end
-last_edge = int(len(similarities) * 0.01)
-similarities[-last_edge:] = similarities[-last_edge]
+ranked_segments = top_segments(query, pdf_name, top_n=3)
 
-# Denoise salience scores
-d_similarities = denoise_similarities(similarities)
-d_similarities -= d_similarities.min()  # normalize
-d_similarities /= d_similarities.max()
-d_similarities = tune_percentile(d_similarities, percentile=75)
-
-segs = segments(d_similarities, document)
-ranked_segments = rank(segs, np.max)[-3:]
-
-import matplotlib.pyplot as plt
-plt.plot(similarities)
-plt.plot(d_similarities)
-plt.show()
-
-for x in ranked_segments:
-    print('--------------------------------------------------')
+for x in reversed(ranked_segments):
+    print('\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n')
     print(x)
