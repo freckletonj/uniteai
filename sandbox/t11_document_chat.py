5e6aa1e602b91f53378ab9344aafbaf0803d938f
mobius <mobius@mobius.mobius>
2023-07-18 16:43:20 -0700

    add colorized visualization in html

--- sandbox/t11_document_chat.py
+++ sandbox/t11_document_chat.py
@@ -28,7 +28,7 @@ class Meta:
     window_size: int
     stride: int
     percentile: int
-    pdf_text: str
+    text: str
     embeddings: List[np.ndarray]
     query_instruction: str
     embed_instruction: str
@@ -54,17 +54,21 @@ def embed(xs: List[str]):
 ##################################################
 # PDF
 
-def load_pdf(meta: Meta):
-    ''' Mutate `meta` to include `pdf_text` '''
-    if meta.pdf_text is None:
+def load_doc(meta: Meta):
+    ''' Mutate `meta` to include `text` '''
+    if meta.text is None:
         path = meta.path
         path = os.path.expanduser(path)
-        reader = PdfReader(path)
-        text = ''
-        for page in reader.pages:
-            text += '\n' + page.extract_text()
-        meta.pdf_text = text
-
+        _, ext = os.path.splitext(path)
+        if ext == '.pdf':
+            reader = PdfReader(path)
+            text = ''
+            for page in reader.pages:
+                text += '\n' + page.extract_text()
+            meta.text = text
+        else:
+            with open(path, 'r') as f:
+                meta.text = f.read()
 
 def get_file_name(path):
     full_name = os.path.basename(path)
@@ -91,7 +95,7 @@ def save_pkl(pdf_key, xs):
 def load_embeddings(meta):
     ''' Mutate `meta` to include embeddings. '''
     pdf_key = meta.name
-    document = meta.pdf_text
+    document = meta.text
     window_size = meta.window_size
     stride = meta.stride
     embed_instruction = meta.embed_instruction
@@ -114,7 +118,7 @@ def load_embeddings(meta):
             chunk_e = embed([[embed_instruction, chunk]])
             embeddings.append(chunk_e)
 
-        meta.embedding = embeddings
+        meta.embeddings = embeddings
         save_pkl(pdf_key, embeddings)
 
 
@@ -122,7 +126,7 @@ def similar_tokens(query: str, meta: Meta) -> List[float]:
     '''Compare a `query` to a strided window over a `document`.'''
     embeddings = meta.embeddings
     pdf_key = meta.name
-    document = meta.pdf_text
+    document = meta.text
     window_size = meta.window_size
     stride = meta.stride
     query_instruction = meta.query_instruction
@@ -211,9 +215,9 @@ def denoise_similarities(similarities, window_size=2000, poly_order=2):
     return savgol_filter(similarities, window_size, poly_order)
 
 
-def top_segments(query, pdf_name, top_n):
-    meta = cache[pdf_name]
-    document = meta.pdf_text
+def top_segments(query, doc_name, top_n, visualize=False):
+    meta = cache[doc_name]
+    document = meta.text
     denoise_window_size = meta.denoise_window_size
     denoise_poly_order = meta.denoise_poly_order
     percentile = meta.percentile
@@ -224,8 +228,10 @@ def top_segments(query, pdf_name, top_n):
     similarities[-last_edge:] = similarities[-last_edge]
 
     # Denoise salience scores
-    similarities = tune_percentile(similarities, percentile)
-    d_similarities = denoise_similarities(similarities, denoise_window_size, denoise_poly_order)
+    # similarities = tune_percentile(similarities, percentile)
+    d_similarities = denoise_similarities(similarities,
+                                          denoise_window_size,
+                                          denoise_poly_order)
     d_similarities -= d_similarities.min()  # normalize
     d_similarities /= d_similarities.max()
     d_similarities = tune_percentile(d_similarities, percentile)
@@ -233,14 +239,108 @@ def top_segments(query, pdf_name, top_n):
     segs = segments(d_similarities, document)
     ranked_segments = rank(segs, np.mean)[:top_n]
 
-    import matplotlib.pyplot as plt
-    plt.plot(similarities)
-    plt.plot(d_similarities)
-    plt.show()
+    if visualize:
+        import matplotlib.pyplot as plt
+        plt.plot(similarities)
+        plt.plot(d_similarities)
+        plt.show()
 
-    return ranked_segments
+    return ranked_segments, d_similarities
 
 
+##################################################
+# Visualization
+
+import webbrowser
+from html import escape
+import os
+from typing import List, Tuple
+import numpy as np
+
+
+def hex_to_rgb(hex_color: str):
+    """ Converts a hexadecimal color string to an RGB tuple. """
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+
+def interpolate_color(similarity: float, color):
+    """ Scales an RGB color tuple by a similarity factor. """
+    rgb_color = hex_to_rgb(color)
+    return tuple(similarity * channel for channel in rgb_color)
+
+
+def add_color(a, b):
+    """ Adds two RGB color tuples element-wise. """
+    return tuple(ai + bi for ai, bi in zip(a, b))
+
+
+def rgb_to_hex(rgb):
+    """ Converts an RGB color tuple to a hexadecimal color string. """
+    r,g,b = rgb
+    rgb = (int(r), int(g), int(b))
+    return '#%02x%02x%02x' % rgb
+
+
+def colorize_text(text: str, queries_and_colors_and_similarity: List[Tuple[str, str, np.ndarray]]) -> str:
+    """ Colorizes text based on similarity scores for different queries. """
+    # Initialize an empty list for the HTML parts
+    html_parts = []
+
+    # Get the color names and similarity scores
+    color_names = [item[1] for item in queries_and_colors_and_similarity]
+    similarities_tr = zip(*[item[2] for item in queries_and_colors_and_similarity])  # transposed
+
+    # Loop through the text with the corresponding similarity
+    for similarities, char in zip(similarities_tr, text):
+        # Initialize color as black
+        color = (0, 0, 0)
+        for name, sim in zip(color_names, similarities):
+            color = add_color(color, interpolate_color(sim, name))
+        hex_color = rgb_to_hex(color)
+
+        if char == '\n':
+            html_parts.append('<br>')
+            continue
+        char = escape(char)
+        html_parts.append(f'<span style="color: {hex_color};">{char}</span>')
+
+    return ''.join(html_parts)
+
+
+def create_colorful_html(queries_and_colors_and_similarity: List[Tuple[str, str, np.ndarray]],
+                         document: str) -> None:
+    """ Creates an HTML page with colorized query and document text. """
+    # Colorize the queries and document
+    queries_html = [colorize_text(query, [(query, color, np.ones(len(query)))])
+                    for query, color, self_similarities
+                    in queries_and_colors_and_similarity]
+    document_html = colorize_text(document, queries_and_colors_and_similarity)
+
+    # Combine the HTML strings for the queries and document
+    html = '''
+<head>
+  <style>
+    body {{
+      background-color: #DDDDDD;
+    }}
+  </style>
+</head>
+'''
+    for i, query_html in enumerate(queries_html):
+        html += f'<h1>QUERY {i+1}:</h1>\n<span>{query_html}</span>\n<br><br>\n'
+    html += '<hr>\n<br><br>\n<h1>DOCUMENT:</h1>\n' + document_html
+
+    # Write the HTML string to a temporary file
+    with open('temp.html', 'w') as f:
+        f.write(html)
+
+    # Open the HTML file in the default web browser
+    webbrowser.open('file://' + os.path.realpath('temp.html'))
+
+
+##################################################
+# Ranked Segments
 
 try:
     cache_is_loaded
@@ -252,7 +352,7 @@ except:
             path='~/Documents/misc/bitcoin.pdf',
             window_size=300,
             stride=100,
-            pdf_text=None,
+            text=None,
             embeddings=None,
             query_instruction='Represent the Science question for retrieving supporting documents: ',
             embed_instruction='Represent the Science document for retrieval: ',
@@ -265,35 +365,77 @@ except:
             path='~/Documents/misc/land use and development code.pdf',
             window_size=300,
             stride=100,
-            pdf_text=None,
+            text=None,
             embeddings=None,
             query_instruction='Represent the wikipedia question for retrieving supporting documents: ',
             embed_instruction='Represent the wikipedia document for retrieval: ',
             denoise_window_size=5000,
             denoise_poly_order=2,
             percentile=80,
-        )
+        ),
+        '2001_positive': Meta(
+            name='2001_positive',
+            path='./2001_positive.md',
+            window_size=500,
+            stride=25,
+            text=None,
+            embeddings=None,
+            query_instruction='Represent the book review question for retrieving supporting documents: ',
+            embed_instruction='Represent the book review document for retrieval: ',
+            denoise_window_size=250,
+            denoise_poly_order=3,
+            percentile=80,
+        ),
+        '2001_negative': Meta(
+            name='2001_negative',
+            path='./2001_negative.md',
+            window_size=500,
+            stride=25,
+            text=None,
+            embeddings=None,
+            query_instruction='Represent the book review question for retrieving supporting documents: ',
+            embed_instruction='Represent the book review document for retrieval: ',
+            denoise_window_size=250,
+            denoise_poly_order=3,
+            percentile=80,
+        ),
     }
     for k, m in cache.items():
-        load_pdf(m)
+        load_doc(m)
         load_embeddings(m)
     cache_is_loaded = True
 
-# pdf_name = 'bitcoin'
-# query = 'whats in it for participants to the blockchain?'
-# query = 'how does this protect my anonymity?'
-# query = 'im concerned my hdd isnt big enough'
-# query = 'who contributed to this paper?'
+# # doc_name = 'bitcoin'
+# # query = 'whats in it for participants to the blockchain?'
+# # query = 'how does this protect my anonymity?'
+# # query = 'im concerned my hdd isnt big enough'
+# # query = 'who contributed to this paper?'
 
-pdf_name = 'idaho'
-# query = 'how close can my silver mine be to a farm?'
-# query = 'how do houses on the lake need to be addressed? marine addressing.'
-# query = 'How can I rezone my property? Rezoning.'
-# query = 'What signs can I put on my property?'
-query = 'How does this document define "sign"?'
+# doc_name = 'idaho'
+# # query = 'how close can my silver mine be to a farm?'
+# # query = 'how do houses on the lake need to be addressed? marine addressing.'
+# # query = 'How can I rezone my property? Rezoning.'
+# # query = 'What signs can I put on my property?'
+# query = 'How does this document define "sign"?'
 
-ranked_segments = top_segments(query, pdf_name, top_n=3)
+# ranked_segments, sims = top_segments(query, doc_name, top_n=3)
 
-for x in reversed(ranked_segments):
-    print('\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n')
-    print(x)
+# for x in reversed(ranked_segments):
+#     print('\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n')
+#     print(x)
+
+
+##################################################
+# Colorize document
+
+doc_name = '2001_positive'
+queries = [
+    ('The reviewer loves the book.', '00cc00'),
+    ('The reviewer hates the book.', 'cc0000'),
+]
+query_tups = []
+for (q, c) in queries:
+    _, sims = top_segments(q, doc_name, top_n=0)
+    query_tups.append((q, c, sims))
+
+create_colorful_html(query_tups, cache[doc_name].text)
